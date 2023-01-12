99bcc64a9fbf16618cb5ce59547ebc6fce7e3105
LuthyX <peteradegoke81@gmail.com>
2023-01-12 23:28:34 +0100

    debug

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -50,6 +50,7 @@ function onConnected() {
 function onError(error) {
     connectingElement.textContent = 'Could not connect to WebSocket server. Please refresh this page to try again!';
     connectingElement.style.color = 'red';
+    console.log(error);
 }
 
 
